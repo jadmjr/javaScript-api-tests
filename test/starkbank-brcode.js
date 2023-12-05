3de2bcfc1c0389679b7ccc5e1d16ccc14c7bc843
Julimar <jadmjr@gmail.com>
2023-12-05 15:04:24 -0300

    removing key from code

--- test/starkbank-brcode.js
+++ test/starkbank-brcode.js
@@ -3,16 +3,8 @@ import { before, describe, it } from "mocha";
 const starkbank = require('starkbank');
 var faker = require('faker-br');
 
-let privateKeyContent = `
------BEGIN EC PARAMETERS-----
-BgUrgQQACg==
------END EC PARAMETERS-----
------BEGIN EC PRIVATE KEY-----
-MHQCAQEEINyLtvEkzuGqBOTEu0qFPbMFwN6UfPFGAfbrJs3NJUN3oAcGBSuBBAAK
-oUQDQgAEgBEJQJH5SmnkufXxYIrLRK8WrUsSaVu3Z4b0OgtDmV/usTPye7uBqzgU
-w/IbT1FuttsyVhGE01tmZEg0vL7E5A==
------END EC PRIVATE KEY-----
-`
+let privateKeyContent = process.env.PRIVATE_KEY;
+
 let user = new starkbank.Project({
   environment: 'sandbox',
   id: '6211482849116160',
